d7f82a178a3dbf2e5487c2a03af51713eb68f8af
appeon-dotnet <github@appeon.com>
2025-05-08 16:58:00 +0800

    2025 GA

--- Command_StartLocalWebAPI.bat
+++ Command_StartLocalWebAPI.bat
@@ -1,1 +1,2 @@
-dotnet.exe run --no -build --project C:\Users\Administrator\Source\Repos\PowerServer_salesdemo\ServerAPIs\ServerAPIs.csproj
+dotnet.exe run --no -build --project C:\Users\Administrator\Source\Repos\ps_salesdemo\ServerAPIs\ServerAPIs.csproj
+Pause
